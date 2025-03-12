dcebc326011f2ca94ba790f134a4131be84f2331
Blueye21 <danjdoh@gmail.com>
2025-03-12 16:59:53 -0700

    Delete lua directory

--- lua/autorun/portal2fix_init.lua
+++
@@ -1,3 +0,0 @@
-include("portal2/portal2_sounds.lua") // Soundscripts
-include("portal2/portal2_particles.lua") // Particles
-include("portal2/portal2_hooks.lua") // Hooks
--- lua/entities/point_changelevel.lua
+++
@@ -1,9 +0,0 @@
-AddCSLuaFile()
-ENT.Type = "point"
-ENT.Base = "base_point"
-
-function ENT:AcceptInput(name, activator, caller, data)
-    if name == "ChangeLevel" then
-        RunConsoleCommand("changelevel",data) 
-    end
-end
